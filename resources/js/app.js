1dc447955a5dc97433be03f97a6ec69522c4a6f4
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-04-12 16:18:19 +0200

    Added bootstrap dependency

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,6 @@ import '~resources/scss/app.scss';
 import.meta.glob([
     '../img/**'
 ]);
+import '~bootstrap/js/index.esm.js'
 import '~resources/scss/app.scss';
 import * as bootstrap from 'bootstrap';
